128be2b427c381538e7788b9d2f137a3bded66fa
chenhao <chenhao03@xiaoduotech.com>
2026-03-12 11:43:09 +0800

    docker 部署

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["better-sqlite3"],
 };
 
 export default withNextIntl(nextConfig);
